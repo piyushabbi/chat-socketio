1dc1c47e3fb128d4534a8c254ebe89e784756a97
Piyush Abbi <piyushabbi27@gmail.com>
2017-11-19 17:08:06 +0530

    Server Side Done

--- server.js
+++ server.js
@@ -23,6 +23,11 @@ io.sockets.on('connection', (socket) => {
 
     // Disconnect
     socket.on('disconnect', () => {
+        // if(!socket.userName) {
+        //     return;
+        // }
+        users.splice(users.indexOf(socket), 1);
+        updateUserNames();
         connections.splice(connections.indexOf(socket), 1);
         console.log(`Disconnected Socket. ${connections.length} socket(s) remaining.`);
     });
@@ -30,7 +35,21 @@ io.sockets.on('connection', (socket) => {
     // Send Message
     socket.on('send message', (data) => {
         io.sockets.emit('new message', {
-            msg: data
+            msg: data,
+            user: socket.userName
         })
     });
+
+    // New User
+    socket.on('new user', (data, callback) => {
+        callback(true);
+        socket.userName = data;
+        users.push(socket.userName);
+        updateUserNames();
+    });
+
+    // Update UserNames
+    function updateUserNames () {
+        io.sockets.emit('get users', users)
+    }
 });
