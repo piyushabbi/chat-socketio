e2de04597128454cbc7e46e00dd45db811784083
Piyush Abbi <piyushabbi27@gmail.com>
2017-11-19 16:32:26 +0530

    Markup added

--- server.js
+++ server.js
@@ -1,7 +1,36 @@
 const express = require('express');
 const http = require('http');
-const io = require('socket.io');
+const socketIO = require('socket.io');
 
 const app = express();
-const server = http.createServer();
+const server = http.createServer(app);
+const io = socketIO.listen(server);
+const port = process.env.port || 3000;
 
+let users = [];
+let connections = [];
+
+server.listen(port);
+console.log('Server running on port ', port);
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`);
+});
+
+io.sockets.on('connection', (socket) => {
+    connections.push(socket);
+    console.log(`Connected ${connections.length} sockets`);
+
+    // Disconnect
+    socket.on('disconnect', () => {
+        connections.splice(connections.indexOf(socket), 1);
+        console.log(`Disconnected Socket. ${connections.length} socket(s) remaining.`);
+    });
+
+    // Send Message
+    socket.on('send message', (data) => {
+        io.sockets.emit('new message', {
+            msg: data
+        })
+    });
+});
